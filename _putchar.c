7ce252779094204b91d6f07a002c404391cfb387
RaniaHamouda <raniahamouda19999@gmail.com>
2023-11-14 12:29:54 -0800

    task0

---
+++ _putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - putchar
+ * Return: number of characters printed
+ * @c: input
+ */
+int _putchar(int c)
+{
+	return (write(1, &c, 1));
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int _putchar(int c);
+int print(char len,...);
+
+
+
+#endif
